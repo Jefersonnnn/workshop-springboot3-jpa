690acd77d773ba40f526edebfcd37dcbdb808f0d
Jefersonnnn <jeferson_m94@hotmail.com>
2023-12-23 14:28:13 -0300

    Many-to-many association with JoinTable

--- src/main/java/com/jm/course/config/TestConfig.java
+++ src/main/java/com/jm/course/config/TestConfig.java
@@ -49,6 +49,14 @@ public class TestConfig implements CommandLineRunner {
         categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
         productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
 
+        p1.getCategories().add(cat2);
+        p2.getCategories().addAll(Arrays.asList(cat1, cat3));
+        p3.getCategories().add(cat3);
+        p4.getCategories().add(cat3);
+        p5.getCategories().add(cat2);
+
+        productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
         User u1 = new User(null, "Jeferson Machado", "jeferson@james.com.br", "988888888", "123456");
         User u2 = new User(null, "Mari Ribeiro", "mari.ribeiro@james.com.br", "988888888", "654321");
 
