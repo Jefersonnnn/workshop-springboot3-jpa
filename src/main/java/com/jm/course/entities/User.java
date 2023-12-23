0ec0c692d9837bc4e32e4d45072a06fa1be75a13
Jefersonnnn <jeferson_m94@hotmail.com>
2023-12-22 22:54:50 -0300

    Order, Instant, ISO 8601

--- src/main/java/com/jm/course/entities/User.java
+++ src/main/java/com/jm/course/entities/User.java
@@ -1,8 +1,11 @@
 package com.jm.course.entities;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.*;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Entity
@@ -17,6 +20,10 @@ public class User implements Serializable {
     private String email;
     private String phone;
     private String password;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "client")
+    private List<Order> orders = new ArrayList<>();
 
     public User() {
     }
@@ -72,6 +79,10 @@ public class User implements Serializable {
     public User setPassword(String password) {
         this.password = password;
         return this;
+    }
+
+    public List<Order> getOrders() {
+        return orders;
     }
 
     @Override
